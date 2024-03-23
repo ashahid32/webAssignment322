f265de43a76e2d74087d8fdd0204f5ea36d3d836
ashahid32 <157094478+ashahid32@users.noreply.github.com>
2024-03-23 11:33:53 -0400

    Update server.js

--- server.js
+++ server.js
@@ -6,19 +6,19 @@
 * 
 *  Name: ___Ayesha Shahid___ Student ID: 145688222 Date: Mar 8, 2024
 *
-*  Online (Cyclic) Link: https://aggressive-calf-jodhpurs.cyclic.app/
+*  Online (Cyclic) Link:
 *
 ********************************************************************************/
 const unCountryData = require("./modules/unCountries");
-const path = require("path");
+//const path = require("path");
+
 const express = require('express');
 const app = express();
-
-
 
 const HTTP_PORT = process.env.PORT || 8080;
 
 app.use(express.static('public')); 
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use((req,res,next)=>{
   app.locals.currentRoute = req.path;
@@ -34,14 +34,50 @@ app.get('/about', (req, res) => {
 });
 
 
+
 app.get('/countries', (req, res) => {
   res.render('countries');
 });
 
-
 app.get('/country', (req, res) => {
   res.render('country');
 });
+
+app.get('/un/addCountry', (req, res) => {
+  unCountryData.getAllRegions().then((regions)=>{return res.render('addCountry',{regions:regions});} )
+});
+
+
+app.post('/un/addCountry', (req, res)=> {
+  unCountryData.addCountry(req.body)
+    .then(() => {
+      return unCountryData.getAllCountries();
+    })
+    .then((countryData) => {
+      res.render('countries', { region: "All", countries: countryData });
+    })
+    .catch((error) => {
+      res.render('500', { message: `I'm sorry, but we have encountered the following error: ${error}` });
+    });
+});
+
+app.post('/un/editCountry/:code', (req, res)=> {
+  unCountryData.getCountryByCode(req.params.code)
+    .then((country) => {
+      return unCountryData.editCountry(country.a2code, req.body); 
+    })
+    .then(() => {
+      return unCountryData.getAllCountries();
+    })
+    .then((countryData) => {
+      res.render('countries', { region: "All", countries: countryData });
+    })
+    .catch((error) => {
+      res.render('500', { message: `I'm sorry, but we have encountered the following error: ${error}` });
+    });
+});
+
+
 
 app.get("/un/countries", async (req,res)=>{
  
@@ -51,7 +87,7 @@ app.get("/un/countries", async (req,res)=>{
       let {region} = req.query;
       region = region.charAt(0).toUpperCase() + region.slice(1);
       if(countries.length)res.render("countries", {region, countries});
-      else res.status(404).render("404", {message: "No Countries found for a matching region!"}) 
+      else throw new Error("No Countries found for a matching region!!")
   
     }else{
       let countries = await unCountryData.getAllCountries();
@@ -64,24 +100,46 @@ app.get("/un/countries", async (req,res)=>{
 
 });
 
-app.get("/un/countries/:code", async (req, res) => {
-  try {
-    const country = await unCountryData.getCountryByCode(req.params.code);
-    res.render("country", { country });
-  } catch (error) {
-    res.render("404", { message: error });
+app.get("/un/deleteCountry/:code", (req, res)=>{
+  unCountryData.deleteCountry(req.params.code)
+  .then(()=>{return unCountryData.getAllCountries();
+  })
+  .then((countryData) => {
+   res.render('countries', { region: "All", countries: countryData });
+  })
+  .catch((error) => {
+    res.render('500', { message: `I'm sorry, but we have encountered the following error: ${error}` });
+  })
   }
-});
+  );
+
+
+  app.get("/un/countries/:code", async (req,res)=>{
+    try{
+      let country = await unCountryData.getCountryByCode(req.params.code);
+      res.render("country", {country});
+     
+    }catch(err){
+      res.render("404", {message: err});
+    }
+  });
+
+
+
+
+app.get("/un/editCountry/:code", (req,res)=>{
+unCountryData.getCountryByCode(req.params.code)
+.then((country)=>{res.render("editCountry", {country})})
+.catch((err)=>res.status(404).render('404', {message: err}) )
+})
 
 
 
 app.use((req, res, next) => {
-  res.status(404).render("404",
-   {message: "I'm sorry, we're unable to find what you're looking for"});
+  res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for"});
 });
 
 unCountryData.initialize().then(()=>{
-  app.listen(HTTP_PORT, () => 
-  { console.log(`server listening on: ${HTTP_PORT}`) });
+  app.listen(HTTP_PORT, () => { console.log(`server listening on: ${HTTP_PORT}`) });
 });
 
