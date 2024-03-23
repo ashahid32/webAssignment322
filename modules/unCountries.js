48957b85c9941e2ca1c541b0f9f9ef8d8e570d58
ashahid32 <157094478+ashahid32@users.noreply.github.com>
2024-03-23 11:37:18 -0400

    Update unCountries.js

--- modules/unCountries.js
+++ modules/unCountries.js
@@ -1,55 +1,169 @@
-const countryData = require("../data/countryData");
-const regionData = require("../data/regionData");
-  
-let countries = [];
+//Assignment 5
+require('dotenv').config();
+const Sequelize = require('sequelize');
+let sequelize=new Sequelize('SenecaDB', 'SenecaDB_owner', 'mD8Ki6duXcnV', {
+  host: 'ep-rough-heart-a5lktagu.us-east-2.aws.neon.tech',
+  dialect: 'postgres',
+  port: 5432,
+  dialectOptions: {
+    ssl: { rejectUnauthorized: false },
+  },
+});
 
+
+
+
+
+//defining a "Region" model
+const Region = sequelize.define('Region', {
+  id:  {
+    type: Sequelize.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: Sequelize.STRING,
+  subs: Sequelize.STRING
+},
+{
+  createdAt: false, 
+  updatedAt: false, 
+}
+
+);
+
+const Country = sequelize.define( 'Country',{
+    a2code:{
+      type: Sequelize.STRING,
+      primaryKey: true,
+    },
+    name:Sequelize.STRING,
+    official: Sequelize.STRING,
+    nativeName: Sequelize.STRING,
+    permanentUNSC: Sequelize.BOOLEAN,
+    wikipediaURL: Sequelize.STRING,
+    capital: Sequelize.STRING,
+    regionId: Sequelize.INTEGER,
+    languages: Sequelize.STRING,
+    population: Sequelize.INTEGER,
+    flag: Sequelize.STRING
+},
+{
+  createdAt: false, 
+  updatedAt: false, 
+}
+);
+
+//association
+Country.belongsTo(Region, {foreignKey: 'regionId'});
+
+
+ //deleted by Assignemnt 5//
+//const countryData = require("../data/countryData");
+//const regionData = require("../data/regionData");
+
+//deleted by assignment 5
+//let countries = [];
+
+//Updated
 function initialize() {
-  return new Promise((resolve, reject) => {
-    countryData?.forEach(country => {
-      let countryWithRegion = { ...country, region: regionData.find(region => region.id == country.regionId) }
-      countries.push(countryWithRegion);
-      resolve();
-    });
-  });
+  return sequelize.sync().then(() => {
+        console.log("Database synchronized successfully");      
+    }).catch((error) => {
+        console.error("Error synchronizing database:", error);
+        throw error; // Reject the promise with the error
+      });
+  }
 
-}
- 
+
+
+
+
 function getAllCountries() {
-  return new Promise((resolve, reject) => {
-    resolve(countries);
-  });
+  return Country.findAll({ include: [Region] })}
+
+
+function getAllRegions(){
+  return Region.findAll();
 }
+
+
 
 function getCountryByCode(countryCode) {
-
-  return new Promise((resolve, reject) => {
-    let foundCountry = countries.find(c => c.a2code == countryCode.toUpperCase());
-
-    if (foundCountry) {
-      resolve(foundCountry)
-    } else {
-      reject("Unable to find requested country");
-    }
-
-  });
-
+  return Country.findOne({  include: [Region], where: { a2code: countryCode.toUpperCase() }});
 }
+
+
+
+function addCountry(countryData){
+  return Country.create({
+    a2code:countryData.a2code,
+    name:countryData.name,
+    official: countryData.official,
+    nativeName: countryData.nativeName,
+    permanentUNSC: (countryData.permanentUNSC)=="on",
+    wikipediaURL: countryData.wikipediaURL,
+    capital: countryData.capital,
+    regionId: countryData.regionId,
+    languages: countryData.languages,
+    population: countryData.population,
+    flag: countryData.flag
+  });
+ 
+}
+
+
+function editCountry(countryCode, countryData) {
+  return new Promise((resolve, reject) => {
+    Country.update({a2code:countryData.a2code,
+      name:countryData.name,
+      official: countryData.official,
+      nativeName: countryData.nativeName,
+      permanentUNSC: (countryData.permanentUNSC)=="on",
+      wikipediaURL: countryData.wikipediaURL,
+      capital: countryData.capital,
+      regionId: countryData.regionId,
+      languages: countryData.languages,
+      population: countryData.population,
+      flag: countryData.flag}, {
+      where: { a2code: countryCode }
+    })
+    .then(() => {
+      resolve();
+    })
+    .catch((error) => {
+      reject(error.errors[0].message);
+    });
+  });
+}
+
+
 
 function getCountriesByRegion(region) {
+  region= region.substring(0,1).toUpperCase() + region.substring(1).toLowerCase();
+  return Country.findAll({
+    include: [Region],
+    where: {'$Region.name$': region}
+  })
+};
 
+
+function deleteCountry(countryCode) {
   return new Promise((resolve, reject) => {
-    let foundCountries = countries.filter(c => c.region.name.toUpperCase().includes(region.toUpperCase()));
-
-    if (foundCountries) {
-      resolve(foundCountries)
-    } else {
-      reject("Unable to find requested countries");
-    }
-
+    Country.destroy({
+      where: {
+        a2code: countryCode
+      }
+    })
+    .then(() => {
+      resolve(); 
+    })
+    .catch((error) => {
+      reject(error.errors[0].message); 
+    });
   });
-
 }
 
-module.exports = { initialize, getAllCountries, getCountryByCode, getCountriesByRegion }
 
+
+module.exports = { initialize, getAllCountries, getCountryByCode, getCountriesByRegion, addCountry, getAllRegions, editCountry, deleteCountry }
 
